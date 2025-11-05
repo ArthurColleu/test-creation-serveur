2b4ba2dc686a4a401395a94e0e60f2418a3c1035
Colleu Arthur <colleuarthur@gmail.com>
2025-11-05 09:44:00 +0100

    les "if" on changer

--- testCreatedServer.ts
+++ testCreatedServer.ts
@@ -7,12 +7,6 @@ const server = http.createServer((req, res) => {
     // Définir le header de réponse
     res.setHeader('Content-Type', 'application/json');
 
-    // Route pour tous les contacts
-    if (req.url === '/contacts' || req.url === '/') {
-        res.writeHead(200);
-        res.end(JSON.stringify(contacts));
-        return;
-    }
     // Redirection de /contacts/ vers /contacts
     if(req.url === '/contacts/'){
         res.writeHead(301, { 'Location': '/contacts' });
@@ -20,6 +14,34 @@ const server = http.createServer((req, res) => {
         return;
     }
     // Route pour ajouter un nouveau contact
+    if (req.url === '/contacts') {
+        // Route pour tous les contacts
+        if ( req.method === 'GET') {
+            res.writeHead(200);
+            res.end(JSON.stringify(contacts));
+            return;
+        } else if (req.method === 'POST') {
+            //Récupére le body de la requête contenant le nouveau contact
+            let body = '';
+
+            req.on('data', chunk => {
+                body += chunk.toString();
+            });
+            req.on('end', () => {
+                const newContact = JSON.parse(body);
+                newContact.id = contacts.length + 1;
+                contacts.push(newContact);
+                fs.writeFileSync('./contacts.json', JSON.stringify(contacts, null, 2));
+                res.writeHead(201);
+                res.end(JSON.stringify(contacts));
+            });
+            return;
+        } else {
+            res.writeHead(405);
+            res.end(JSON.stringify({ error: 'Method Not Allowed' }));
+            return;
+        }
+    }
     
     
     // Route pour un contact spécifique : /contacts/1 par exemple
